6d72178f625dd519d0471b10d2b08522007c6feb
Alberto Turrion <albertoturriondiez@gmail.com>
2021-11-13 12:39:13 +0100

    home-page unlogged

--- venv/app.py
+++ venv/app.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, url_for
 import requests
 import os
 from tools import api_data, get_lemmas
@@ -16,7 +16,7 @@ app.secret_key = "myapp"
 @app.route("/", methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
-        return render_template('hello.html')
+        return render_template('index.html')
     else:
         try:
             word = request.form['word']
@@ -24,7 +24,7 @@ def index():
             lemmas = get_lemmas(word)
             if not isinstance(lemmas, list):
                 flash(lemmas)
-                return redirect("/")
+                return redirect(url_for("index"))
             return render_template('results.html', word=word, data=data, lemmas=lemmas)
         except AssertionError as error:
             return(f"{error}")
